de30707b472a738b86fc78c73e1b8e6640e81fc3
ehcodes <engrid.hernandez@gmail.com>
2023-09-18 17:16:21 -0400

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get("/pokemon", (req, res) => {
   res.render('Index',{pokemon: pokemon});
 });
 
+app.get("/pokemon/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
 
 app.listen(port, () => {
   console.log(`See ${siteAddress} for pokemon data.`);
