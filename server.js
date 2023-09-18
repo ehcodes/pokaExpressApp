c642b9f1d1ce6e138ff84868b90e5601db081d55
ehcodes <engrid.hernandez@gmail.com>
2023-09-18 13:52:12 -0400

    connected db, tested json visibility

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const siteAddress = `http://localhost:3000/`;
+const pokemon = require("./models/pokemon.js");
 
-  app.get("/", (req, res) => {
-    res.send('Welcome to the Pokemon App!');
-  });
+app.get("/", (req, res) => {
+  res.send('Welcome to the Pokemon App!');
+});
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
+});
+
 
 app.listen(port, () => {
   console.log(`See ${siteAddress} for pokemon data.`);
