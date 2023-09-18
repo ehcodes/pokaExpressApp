2875a9326af03f077f532d61a07ab84ad55fe67d
ehcodes <engrid.hernandez@gmail.com>
2023-09-18 17:09:14 -0400

    view a list of pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,20 +1,33 @@
 const React = require("react");
-const lazyCSS =
-{
-  textAlign:'center',
-  color: '#ffffff',
-  backgroundColor: '#000000'
+const lazyCSS = {
+  textAlign: "center",
+  color: "#ffffff",
+  backgroundColor: "#000000",
+};
+const ulStyle = {};
+const liStyle = {
+  listStyle: "none",
 };
 
 class Index extends React.Component {
   render() {
+    const { pokemon } = this.props;
+    const capitalize = (str) => {
+      str = str.toUpperCase().slice(0, 1).concat(str.slice(1));
+      return str;
+    };
     return (
-        <>
-          <section style={lazyCSS}>
-            <h1>See some of the pokemon!</h1>
-          </section>
-        </>
-      );
+      <>
+        <section style={lazyCSS} className="pokemonAplenty">
+          <h1>See Some Pokemon!</h1>
+          <ul style={ulStyle}>
+            {pokemon.map((pokemon, i) => {
+              return <li style={liStyle}>{capitalize(pokemon.name)}</li>;
+            })}
+          </ul>
+        </section>
+      </>
+    );
   }
 }
 
