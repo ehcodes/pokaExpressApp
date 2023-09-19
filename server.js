6b29bf83dd537059279e52be6a1c5a9bd463161d
ehcodes <engrid.hernandez@gmail.com>
2023-09-19 12:10:10 -0400

    added new routes

--- server.js
+++ server.js
@@ -8,18 +8,32 @@ const jsxViewEngine = require("jsx-view-engine");
 app.set("view engine", "jsx");
 app.engine("jsx", jsxViewEngine());
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send('Welcome to the Pokemon App!');
+  res.send('Welcome to the <a href="/pokemon">Pokemon</a> App!');
 });
 
 app.get("/pokemon", (req, res) => {
   res.render('Index',{pokemon: pokemon});
 });
 
+app.get("/pokemon/new", function (req, res) {
+  res.render("New");
+});
+
+app.post("/pokemon", (req, res) => {
+  pokemon.push(req.body);
+  res.redirect('/pokemon');
+});
+
 app.get("/pokemon/:id", (req, res) => {
   res.render('Show',{pokemon: pokemon[req.params.id]});
 });
-
 
 app.listen(port, () => {
   console.log(`See ${siteAddress} for pokemon data.`);
