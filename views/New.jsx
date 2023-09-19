c9d7e5d1489c6d5e8a399d763d1da4b701753ad4
ehcodes <engrid.hernandez@gmail.com>
2023-09-19 12:37:07 -0400

    added New component

---
+++ views/New.jsx
@@ -0,0 +1,21 @@
+const React = require('react');
+
+class New extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>New Pokemon page</h1>
+            {/* 
+            action = server route
+            method = HTTP verb 
+            */}
+            <form action="/pokemon" method="POST">
+              Name: <input type="text" name="name" /><br/>
+              Img: <input type="text" name="img" /><br/>
+              <input type="submit" name="" value="Create Pokemon"/>
+            </form>
+        </div>);
+    }
+  }
+
+module.exports = New;
