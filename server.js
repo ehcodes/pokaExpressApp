c4d1561f289607e4e3f5ce7c31598f7c8c962ed9
ehcodes <engrid.hernandez@gmail.com>
2023-09-21 09:48:43 -0400

    updated routs to use mongoose

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const port = 3000;
-const siteAddress = `http://localhost:3000/`;
-const pokemon = require("./models/pokemon.js");
+
+const Pokemon = require("./models/PokemonSchema.js");
 
 const jsxViewEngine = require("jsx-view-engine");
 app.set("view engine", "jsx");
 app.engine("jsx", jsxViewEngine());
+
+const mongoose = require("mongoose");
+
+// DB CONNECTION
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,21 +32,41 @@ app.get("/", (req, res) => {
 });
 
 app.get("/pokemon", (req, res) => {
-  res.render('Index',{pokemon: pokemon});
+  Pokemon.find({}).then((allPokemon) => {
+    res.render("Index", {
+      pokemon: allPokemon,
+    });
+  });
 });
 
 app.get("/pokemon/new", function (req, res) {
   res.render("New");
 });
 
-app.post("/pokemon", (req, res) => {
-  pokemon.push(req.body);
-  res.redirect('/pokemon');
+app.post("/pokemon", async function (req, res) {
+  try {
+    const newPokemon = await Pokemon.create(req.body);
+    return res.redirect("/pokemon");
+  } catch (err) {
+    res.send(`error in adding pokemon`);
+    console.error(err);
+  }
 });
 
-app.get("/pokemon/:id", (req, res) => {
-  res.render('Show',{pokemon: pokemon[req.params.id]});
+app.get("/pokemon/:id", async function (req, res) {
+  try {
+    const foundPokemon = await Pokemon.findById(req.params.id);
+    res.render("Show", {
+      pokemon: foundPokemon,
+    });
+  } catch (err) {
+    res.send(err);
+    console.error(err);
+  }
 });
+
+const port = 3000;
+const siteAddress = `http://localhost:3000/`;
 
 app.listen(port, () => {
   console.log(`See ${siteAddress} for pokemon data.`);
